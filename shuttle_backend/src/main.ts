e5a48a0f9381f059a27232bf83ac8eeb9388d5e0
kurotsuchi <126145510+hitsuga13@users.noreply.github.com>
2026-08-26 03:02:47 +0800

    last step untuk deploy BE

--- shuttle_backend/src/main.ts
+++ shuttle_backend/src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:9000', 'http://localhost:9001'],
+    origin: [
+      'http://localhost:9000',
+      'http://localhost:9001',
+      'https://hitsuga13.github.io',
+    ],
     credentials: true,
   });
   //await app.listen(process.env.PORT ?? 3000);
